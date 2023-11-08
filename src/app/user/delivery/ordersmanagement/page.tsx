de6d1155bbf8758c804e788101f0dddd8f7cc387
Nicolas Müller <nicolasmatiasmuller19@gmail.com>
2023-11-08 10:01:18 -0300

    deplot 3

--- src/app/user/delivery/ordersmanagement/page.tsx
+++ src/app/user/delivery/ordersmanagement/page.tsx
@@ -66,7 +66,7 @@ const Page = () => {
       <h3 className="text-2xl font-normal mt-10">Ordenes pendientes</h3>
       {orders.length > 0 ? (
         orders.map((o) => (
-          <div className="flex justify-between border border-black rounded-xl mt-5 max-w-3xl p-5">
+          <div key={o.id} className="flex justify-between border border-black rounded-xl mt-5 max-w-3xl p-5">
             <div className="flex justify-center align-middle flex-col">
               <p className="text-xl">Id del pedido: </p>
               <p>{o.id}</p>
